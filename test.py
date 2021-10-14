c689e38b2c7683d67c34f129a44e0f63725c6ca2
datagod <william.mcevoy@gmail.com>
2021-10-14 01:54:47 -0400

    Extra running man

--- test.py
+++ test.py
@@ -27,13 +27,15 @@ print ("")
 
 
 
-
 #--------------------------------------
 #  SHOW TITLE SCREEN                 --
 #--------------------------------------
 
 
 while (1==1):
+
+
+
 
 
   #This allows you to create a title screen with different size text
@@ -67,13 +69,18 @@ while (1==1):
   LED.ShowGlowingText(CenterHoriz=True,CenterVert=False,h=0,v=12,Text='SPRITES',RGB=LED.HighCyan,ShadowRGB=LED.ShadowCyan,ZoomFactor= 2,GlowLevels=25,DropShadow=True)
   time.sleep(1)
 
-  for x in range(1,10):
+  for x in range(1,25):
     #Currently there are 27 colorful "bright" RGB values, stored in BrightColorList[].
     GhostRGB = LED.BrightColorList[random.randint(1,LED.BrightColorCount)]
     LED.CopySpriteToPixelsZoom(LED.BlueGhostSprite,random.randint(5,45),random.randint(0,20), ColorTuple=GhostRGB,ZoomFactor=random.randint(1,6),Fill=False)
-    time.sleep(0.25)
+    time.sleep(0.05)
     LED.CopySpriteToPixelsZoom(LED.PacSprite,random.randint(5,45),random.randint(0,20), ColorTuple=LED.HighYellow,ZoomFactor=random.randint(1,2),Fill=False)
-    time.sleep(0.25)
+    time.sleep(0.05)
+
+  #Zoom fade screen
+  LED.ZoomScreen(LED.ScreenArray,32,62,Fade=False,ZoomSleep=0.001)
+  LED.ZoomScreen(LED.ScreenArray,62,1,Fade=True,ZoomSleep=0.0)
+
 
 
   #--------------------------------------
@@ -92,6 +99,9 @@ while (1==1):
   LED.ShowGlowingSprite(CenterHoriz=True,CenterVert=False,h=0,v=0,TheSprite=LED.BlueGhostSprite,RGB=LED.HighOrange,ShadowRGB=LED.ShadowOrange,ZoomFactor= 5,GlowLevels=100,FadeLevels=50,DropShadow=True,FadeDelay=0)
   LED.ShowGlowingSprite(CenterHoriz=True,CenterVert=False,h=0,v=0,TheSprite=LED.BlueGhostSprite,RGB=LED.HighOrange,ShadowRGB=LED.ShadowOrange,ZoomFactor= 6,GlowLevels=100,FadeLevels=50,DropShadow=True,FadeDelay=0)
 
+  #Zoom fade screen
+  LED.ZoomScreen(LED.ScreenArray,32,52,Fade=False,ZoomSleep=0.005)
+  LED.ZoomScreen(LED.ScreenArray,42,1,Fade=True,ZoomSleep=0.0)
 
 
   #--------------------------------------
@@ -127,17 +137,21 @@ while (1==1):
   time.sleep(1)
 
   LED.DotZerkRobot.LaserScan(h1=5,v1=24)
-  LED.DotZerkRobotWalkingSmall.LaserScan(h1=28,v1=27)
-  LED.ChickenRunning.LaserScan(h1=45,v1=24)
+  LED.DotZerkRobotWalkingSmall.LaserScan(h1=15,v1=27)
+  LED.ChickenRunning.LaserScan(h1=26,v1=25)
+
 
   LED.DotZerkRobot.LaserErase(h1=5,v1=24)
-  LED.DotZerkRobotWalkingSmall.LaserErase(h1=28,v1=27)
-  LED.ChickenRunning.LaserErase(h1=45,v1=24)
+  LED.DotZerkRobotWalkingSmall.LaserErase(h1=15,v1=27)
+  LED.ChickenRunning.LaserErase(h1=26,v1=25)
   time.sleep(1)
+
+  
 
   #Zoom fade screen
   LED.ZoomScreen(LED.ScreenArray,32,42,Fade=False,ZoomSleep=0.015)
   LED.ZoomScreen(LED.ScreenArray,42,1,Fade=True,ZoomSleep=0.0)
+
 
 
 
@@ -176,32 +190,31 @@ while (1==1):
 
 
 
-  Frames = LED.RunningManSprite.frames
+
 
   LED.ClearBuffers() #clean the internal graphic buffers
-  for x in range (1,10):
+  LED.RunningMan2Sprite.ScrollAcrossScreen(20,15,'right',0.03 )
+  LED.RunningMan2Sprite.HorizontalFlip()
+  LED.RunningMan2Sprite.ScrollAcrossScreen(20,15,'left',0.02 )
+  LED.RunningMan2Sprite.HorizontalFlip()
+  LED.RunningManSprite.ScrollAcrossScreen(20,15,'right',0.01 )
+  LED.RunningManSprite.HorizontalFlip()
+  LED.RunningManSprite.ScrollAcrossScreen(20,15,'left',0 )
+  LED.RunningManSprite.HorizontalFlip()
+  time.sleep(0.5)
+
+  Frames = LED.RunningManSprite.frames
+  LED.ClearBuffers() #clean the internal graphic buffers
+  for x in range (1,25):
     for i in range (1,Frames+1):
       LED.RunningManSprite.currentframe = i
       LED.CopyAnimatedSpriteToPixelsZoom(LED.RunningManSprite,h=0,v=0, ZoomFactor=2)
-      LED.CopyAnimatedSpriteToPixelsZoom(LED.RunningManSprite,h=45,v=15, ZoomFactor=1)
+      LED.CopyAnimatedSpriteToPixelsZoom(LED.RunningMan2Sprite,h=40,v=14, ZoomFactor=1)
       time.sleep(0.05)
 
   #Zoom fade screen
   LED.ZoomScreen(LED.ScreenArray,32,52,Fade=False,ZoomSleep=0.005)
   LED.ZoomScreen(LED.ScreenArray,42,1,Fade=True,ZoomSleep=0.0)
-
-
-  LED.ClearBuffers() #clean the internal graphic buffers
-  LED.RunningManSprite.ScrollAcrossScreen(0,15,'right',0.01 )
-  LED.RunningManSprite.HorizontalFlip()
-  LED.RunningManSprite.ScrollAcrossScreen(0,15,'left',0.008 )
-  LED.RunningManSprite.HorizontalFlip()
-  LED.RunningManSprite.ScrollAcrossScreen(0,15,'right',0.005)
-  LED.RunningManSprite.HorizontalFlip()
-  LED.RunningManSprite.ScrollAcrossScreen(0,15,'left',0 )
-  LED.RunningManSprite.HorizontalFlip()
-  time.sleep(1)
-
 
 
 
